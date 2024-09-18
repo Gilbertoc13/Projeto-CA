6dcb5c836a68a5043cdfef5fc154cdd55b3fcacb
Gilbertoc13 <gilbertocassiano91@gmail.com>
2024-09-18 12:57:31 -0300

    .

---
+++ C.A-project/src/main.tsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/login';
+import './index.css';
+import About from './pages/About/about';
+import Logo from './pages/logo/logo';
+import Cadastro from './pages/Cadastro/cadastro'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Logo />} />
+        <Route path="/login" element={<Login />} />
+        <Route path='/cadastro'element={<Cadastro />} />
+        <Route path='/about'element={<About />} />
+        
+      </Routes>
+    </Router>
+  </StrictMode>,
+);
